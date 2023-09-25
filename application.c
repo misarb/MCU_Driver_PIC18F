34b62e1e5615ab2a22240c67e9e6cc2699037d55
misarb <lahssane12@gmail.com>
2023-09-25 20:59:52 +0100

    first commit

---
+++ application.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   application.c
+ * Author: BOULBALAH Lahcen
+ *
+ * Created on September 25, 2023, 8:00 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+
+    return (EXIT_SUCCESS);
+}
+
